a48939d00441631fdb16a1ea3cda6c7934ee8083
Gerard KESSE <gerard.kesse@readydev.com>
2021-04-25 18:16:24 +0200

    Initial Commit

---
+++ app/unix/envs.sh
@@ -0,0 +1,11 @@
+#================================================
+export "GPROJECT_ROOT=/home/rpi4/Programs"
+export "GPROJECT_PATH=${GPROJECT_ROOT}/ReadyRPI"
+export "GPROJECT_SRC=${GPROJECT_PATH}/app/code/readyapp/src"
+export "GPROJECT_DATA=${GPROJECT_PATH}/data"
+#================================================
+export "GSQLITE_DB_PATH=${GPROJECT_DATA}/sqlite/config.dat"
+#================================================
+export "GGIT_URL=https://github.com/gkesse/ReadyRPI"
+export "GGIT_NAME=ReadyRPI"
+#================================================
